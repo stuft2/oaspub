2a3e574efb61b65e32013d6c89fd04f6688d65a9
Spencer Tuft <stuft2@byu.edu>
2021-02-18 04:03:57 -0700

    Update unit tests

--- packages/api/test/unit/account.spec.ts
+++ packages/api/test/unit/account.spec.ts
@@ -2,12 +2,12 @@ import 'mocha'
 import chai = require('chai')
 import chaiAsPromised = require('chai-as-promised')
 import * as sinon from 'sinon'
-import AccountControllers = require('../src/controllers/account')
+import AccountControllers = require('../../src/controllers/account')
 import {Db, MongoError} from 'mongodb'
 import {SinonStubbedInstance} from 'sinon'
 import {Request, Response} from 'express'
-import {HttpStatus} from "../src/util/uapi"
-import {AccountModel} from "../src/db/models"
+import {HttpStatus} from "../../src/util/uapi"
+import {AccountModel} from "../../src/db/models"
 
 chai.use(chaiAsPromised)
 const {expect} = chai
@@ -66,7 +66,6 @@ describe('Account Controllers', () => {
       expect(responseBody).to.have.property('metadata')
     })
     it('should not create an account that already exists', async () => {
-
       const insertOne = sinon.stub().rejects(new MongoError('Error about a duplicate'))
       db.collection.returns({insertOne} as any)
 
@@ -84,6 +83,19 @@ describe('Account Controllers', () => {
       expect(responseBody).to.not.have.property('email')
       expect(responseBody).to.not.have.property('password')
       expect(responseBody).to.have.property('metadata')
+    })
+    it('should throw other errors', async () => {
+      const insertOne = sinon.stub().rejects(Error('Unexpected Error'))
+      db.collection.returns({insertOne} as any)
+      expect(controllers.create(req, res)).to.eventually.be.rejectedWith(Error)
+
+      // check logic flow
+      expect(db.collection.calledOnce).to.equal(true)
+      expect(insertOne.calledOnce).to.equal(true)
+
+      // check actual results
+      expect(res.status.called).to.equal(false)
+      expect(res.send.called).to.equal(false)
     })
   })
 
@@ -139,19 +151,91 @@ describe('Account Controllers', () => {
       }
     })
     it('should update an account', async () => {
-      // TODO - Implement test
-      expect(true).to.equal(false)
+      const findOneAndUpdate = sinon.stub().resolves({value: dbResult})
+      db.collection.returns({findOneAndUpdate} as any)
+
+      await controllers.update(req, res)
+      const [responseBody] = res.send.getCall(0).args
+
+      // check logic flow
+      expect(db.collection.calledOnce).to.equal(true)
+      expect(findOneAndUpdate.calledOnce).to.equal(true)
+
+      // check actual results
+      expect(responseBody).to.have.property('username')
+      expect(responseBody).to.have.property('email')
+      expect(responseBody).to.not.have.property('password')
+      expect(responseBody).to.have.property('metadata')
+      expect(responseBody.metadata.validation_response.code).to.equal(HttpStatus.SUCCESS)
+    })
+    it('should not update an account username or email that already exists', async () => {
+      const findOneAndUpdate = sinon.stub().rejects(new MongoError('Error about a duplicate'))
+      db.collection.returns({findOneAndUpdate} as any)
+
+      await controllers.update(req, res)
+      const [statusCode] = res.status.getCall(0).args
+      const [responseBody] = res.send.getCall(0).args
+
+      // check logic flow
+      expect(db.collection.calledOnce).to.equal(true)
+      expect(findOneAndUpdate.calledOnce).to.equal(true)
+
+      // check actual results
+      expect(statusCode).to.equal(HttpStatus.CONFLICT)
+      expect(responseBody).to.not.have.property('username')
+      expect(responseBody).to.not.have.property('email')
+      expect(responseBody).to.not.have.property('password')
+      expect(responseBody).to.have.property('metadata')
     })
     it('should return 404 for an account that does not exist or is inactive', async () => {
-      // TODO - Implement test
-      expect(true).to.equal(false)
+      const findOneAndUpdate = sinon.stub().resolves({})
+      db.collection.returns({findOneAndUpdate} as any)
+
+      await controllers.update(req, res)
+      const [responseBody] = res.send.getCall(0).args
+
+      // check logic flow
+      expect(db.collection.calledOnce).to.equal(true)
+      expect(findOneAndUpdate.calledOnce).to.equal(true)
+
+      // check actual results
+      expect(responseBody).to.not.have.property('username')
+      expect(responseBody).to.not.have.property('email')
+      expect(responseBody).to.not.have.property('password')
+      expect(responseBody).to.have.property('metadata')
+      expect(responseBody.metadata.validation_response.code).to.equal(HttpStatus.NOT_FOUND)
+    })
+    it('should throw other errors', async () => {
+      const findOneAndUpdate = sinon.stub().rejects(Error('Unexpected Error'))
+      db.collection.returns({findOneAndUpdate} as any)
+      expect(controllers.update(req, res)).to.eventually.be.rejectedWith(Error)
+
+      // check logic flow
+      expect(db.collection.calledOnce).to.equal(true)
+      expect(findOneAndUpdate.calledOnce).to.equal(true)
+
+      // check actual results
+      expect(res.status.called).to.equal(false)
+      expect(res.send.called).to.equal(false)
     })
   })
 
   describe('Deactivating an account', () => {
     it('should mark an account as inactive, regardless of previous state', async () => {
-      // TODO - Implement test
-      expect(true).to.equal(false)
+      const findOneAndUpdate = sinon.stub().resolves({value: dbResult})
+      db.collection.returns({findOneAndUpdate} as any)
+
+      await controllers.deactivate(req, res)
+      const [statusCode] = res.status.getCall(0).args
+      const [responseBody] = res.send.getCall(0).args
+
+      // check logic flow
+      expect(db.collection.calledOnce).to.equal(true)
+      expect(findOneAndUpdate.calledOnce).to.equal(true)
+
+      // check actual results
+      expect(statusCode).to.equal(HttpStatus.NO_CONTENT)
+      expect(responseBody).to.equal(undefined)
     })
   })
 })
